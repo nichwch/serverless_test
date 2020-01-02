ce49420921eadc2b58913a9cd649d6077c574820
Nicholas Chen <nichwch@gmail.com>
2020-01-01 16:42:50 -0800

    commit

---
+++ delete.js
@@ -0,0 +1,22 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be removed
+    // - 'userid': Identity Pool identity id of the authenticated user
+    // - 'noteid': path parameter
+    Key: {
+      userid: event.requestContext.identity.cognitoIdentityId,
+      noteid: event.pathParameters.id
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("delete", params);
+    return success({ status: true });
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
---
+++ list.js
@@ -0,0 +1,26 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userid = :userid': only return items with matching 'userid'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userid': defines 'userid' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "userid = :userid",
+    ExpressionAttributeValues: {
+      ":userid": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    // Return the matching list of items in response body
+    return success(result.Items);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
